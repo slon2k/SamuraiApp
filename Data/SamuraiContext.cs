d556fd48d16e4cd2fc2b9e6f19ad5f1abe112dce
Boris Korobeinikov <slon2k@gmail.com>
2020-02-06 17:03:01 +0600

    Add project files.

---
+++ Data/SamuraiContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Domain;
+
+namespace Data
+{
+    public class SamuraiContext : DbContext
+    {
+   
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Clan> Clans { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = SamuraiAppData");
+            //base.OnConfiguring(optionsBuilder);
+        }
+
+    }
+}
---
+++ Domain/Quote.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain
+{
+    public class Quote
+    {
+        public int Id { get; set; }
+        public Samurai Samurai { get; set; }
+        public string Text { get; set; }
+        public int SamuraiId { get; set; }
+    }
+}
